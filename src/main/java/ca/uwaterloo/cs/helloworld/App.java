d35f946ae12461240cad475d8fe2fe4ace3319c8
PatrickTDavies <davipatr389@gmail.com>
2024-02-05 14:06:12 -0500

    breaking3

--- src/main/java/ca/uwaterloo/cs/helloworld/App.java
+++ src/main/java/ca/uwaterloo/cs/helloworld/App.java
@@ -7,6 +7,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        Syssstem.out.println( "Hello, world!" );
+        Syssssnaketem.out.println( "Hello, world!" );
     }
 }
